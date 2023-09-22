b51e331f0409d1158f65211dcf8aaaffb75bec9e
OliRamos <oli.ramos@ymail.com>
2023-09-22 11:25:07 -0300

    Add project files.

---
+++ FilmesApi/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos;
+
+public class UpdateFilmeDto
+{
+    [Required(ErrorMessage = "Título é obrigatório")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "Gênero é obrigatório")]
+    [StringLength(50, ErrorMessage = "Máximo de 50 caracteres")]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "Duração deve ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}

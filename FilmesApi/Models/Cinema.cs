7dd668bbd4424066b0fd5cdb43cc1e24e64009dc
OliRamos <oli.ramos@ymail.com>
2023-09-22 12:37:24 -0300

    Criada entidade Cinema

--- FilmesApi/Models/Cinema.cs
+++ FilmesApi/Models/Cinema.cs
@@ -1,6 +1,13 @@
-﻿namespace FilmesApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
 {
     public class Cinema
     {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Um nome é necessário")]
+        public string Nome { get; set; }
     }
 }
